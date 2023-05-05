a9f2a6b413cd3cba7b471b47bf65bbecf63d7221
Dari Huallpa <dariojoel152@gmail.com>
2023-05-05 10:51:49 -0600

    - Added exercise

--- exercises.py
+++ exercises.py
@@ -38,13 +38,57 @@ Here are some simple exercises to practice with Python.
 #** 3) Write a Python function to find the highest and lowest number in a list.
 
 
-def find_max_min():
-    myList = [0, 2, 4, 84, 54, 97, -18, -51]
-    theMax = max(myList)
-    theMin = min(myList)
-    print(f"The highest number in the list is {theMax}, and the lowest is {theMin}.")
+# def find_max_min():
+#     myList = [0, 2, 4, 84, 54, 97, -18, -51]
+#     theMax = max(myList)
+#     theMin = min(myList)
+#     print(f"The highest number in the list is {theMax}, and the lowest is {theMin}.")
   
   
-find_max_min()
+# find_max_min()
 
-#** 4) Write a Python program that takes a list of numbers and returns a new list containing only the even numbers.
+#** 4) Write a Python function that takes a list of numbers and returns a new list containing only the even numbers.
+
+
+# def get_even_numbers():
+#     myList = [0, 2, 4, 84, 54, 97, -18, -51, 12, 98]
+#     print(f"Original list: {myList}")
+#     evenNumberList = []
+#     for number in myList:
+#         if number % 2 == 0:
+#             evenNumberList.append(number)
+#     return evenNumberList
+
+
+# print(f"Even numbers extracted: {get_even_numbers()}")
+
+#** 6) Write a Python function to check whether a string is a palindrome or not
+
+
+# def check_palindrome(word):
+#     word = ''.join(char for char in word.lower() if char.isalnum())
+#     return word == word[::-1]
+
+
+# word = input("Enter a word to check if it's a palindrome: ")
+# print(check_palindrome(word))
+
+#** 7) Write a Python program that accepts a string and calculate the number of digits and letters in it.
+
+
+def count_digits_letters(myString):
+    digits = 0
+    letters = 0
+    for char in myString:
+        if char.isdigit():
+            digits += 1
+        elif char.isalpha():
+            letters += 1
+    
+    return digits, letters
+
+
+myString = input("Enter your string: >> ")
+digits, letters = count_digits_letters(myString)
+print(f"Number of digits: {digits}\nNumber of letters: {letters}")
+
