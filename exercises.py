2372e9f5c7199882007c53777edf2f656785e837
Dari Huallpa <dariojoel152@gmail.com>
2023-05-08 12:53:04 -0600

    -Added exercises

--- exercises.py
+++ exercises.py
@@ -155,14 +155,114 @@ Here are some simple exercises to practice with Python.
 #** 12) Write a Python program to find the sum of the digits of a given number.
 
 
-def sum_digits(number):
-    sum = 0
-    for digit in str(number):
-        sum += int(digit)
-    return sum
+# def sum_digits(number):
+#     sum = 0
+#     for digit in str(number):
+#         sum += int(digit)
+#     return sum
 
 
-number = int(input("Enter a number: >> "))
-print(f"The sum of the digits of {number} is: {sum_digits(number)}")
+# number = int(input("Enter a number: >> "))
+# print(f"The sum of the digits of {number} is: {sum_digits(number)}")
+
+#** 13) Write a Python program to count the number of vowels in a given string.
 
 
+# def count_vowels(myString):
+#     vowels = set("aeiouAEIOU")
+#     count = 0
+#     for letter in myString:
+#         if letter in vowels:
+#             count += 1
+#     return count
+
+
+# myString = input("Enter a string: >> ")
+# print(f"\nTotal vowels: {count_vowels(myString)}")
+
+# # Counting each vowel individually
+# myString = myString.lower()
+# print(f"""
+# Total 'A/a' vowels: {myString.count('a')}
+# Total 'E/e' vowels: {myString.count('e')}
+# Total 'I/i' vowels: {myString.count('i')}
+# Total 'O/o' vowels: {myString.count('o')}
+# Total 'U/u' vowels: {myString.count('u')}""")
+
+#** 14) Write a Python program to find the second largest number in a list.
+
+
+# def find_second_highest(list_of_numbers):
+#     if len(list_of_numbers) > 1:
+#         list_of_numbers.sort()
+#         return list_of_numbers[-2]
+#     else:
+#         print("Fill your list at least with 2 elements!")
+#         return None
+        
+# list_of_numbers = [117, 19, 22, 1990, 11, 94, 49, 13, 69, 77]
+# print(f"Original list: {list_of_numbers}")
+# print(f"The second highest number is: {find_second_highest(list_of_numbers)}")
+
+
+#** 15) Write a Python program to find the common elements between two lists.
+
+
+# def find_coincidences_in_lists(list1, list2):
+#     set1 = set(list1)
+#     set2 = set(list2)
+#     coincidences = set1.intersection(set2)
+#     return list(coincidences)
+
+
+# list1 = [19, 88, 91, 34, 65, 110, 38, 88, 34, 34, 10551]
+# list2 = [1, 88, 91, 94, 25, 109, 19, 300, 10551, 784, 5]
+# print(f"Coincidences in both lists: {find_coincidences_in_lists(list1, list2)}")
+
+#** 16) Write a Python program to sort a list of tuples based on the second element of each tuple.
+
+
+# def sort_tuple_by_second_element(list_of_tuples):
+#     """
+#     Function that sorts a list of tuples based on the second element of each tuple. Function 'sorted' is used as well as a lambda function as the 'key' parameter. This parameter is used to specify a function to be called on each element of the iterable that is being sorted. This function returns a value that is used to determine the order of the elements in the sorted result. The 'x' parameter represents each tuple in the list of tuples. The x[1] expression retrieves the second element of each tuple, which is what we want to use for sorting.
+#     """
+#     return sorted(list_of_tuples, key=lambda x: x[1])
+
+
+# list_of_tuples = [(2, 4), (5, 3), (7, 15), (23, 4), (7,8), (1, 1)]
+# print(f"Original list of tuples: {list_of_tuples}")
+# print(f"Sorted list of tuples: {sort_tuple_by_second_element(list_of_tuples)}")
+
+#** 17) Write a Python program to find the GCD (greatest common divisor) of two numbers.
+
+
+# def find_gcd(num1, num2):
+#     if num2 == 0:
+#         return num1
+#     else:
+#         return find_gcd(num2, num1%num2)
+
+
+# num1 = int(input("Enter a number: >> "))
+# num2 = int(input("Enter another number: >> "))
+# print(f"The greatest common divisor of {num1} and {num2} is: {find_gcd(num1, num2)}")
+
+#** 18) Write a Python program to find the sum of all prime numbers between 1 and n (where n is provided by the user)
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+n = int(input("Enter a number: >> "))
+sum_primes = 0
+for i in range(2, n+1):
+    if is_prime(i):
+        sum_primes += i
+        
+print(f"The sum of all primes between 1 and {n} is: {sum_primes}")
