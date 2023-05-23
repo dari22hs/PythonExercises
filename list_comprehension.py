50a3099725a67c694e5ce112b409c40c3383f771
Dari Huallpa <dariojoel152@gmail.com>
2023-05-23 10:31:37 -0600

    - Added exercises

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,17 +1,27 @@
 """
 Collection of simple Python exercises about list comprehension
 """
-#** 1) Write a Python program to square every number in a list.
+#** 1) Square every number in a list.
 # numbers = [1, 2, 3, 4, 5, 6]
 # squared = [n ** 2 for n in numbers]
 # print(squared)
 
-#** 2) Write a Python program to square even numbers in a list.
+#** 2) Square even numbers in a list.
 # numbers = [1, 2, 3, 4, 5, 6]
 # squared = [n ** 2 for n in numbers if n % 2 == 0]
 # print(squared)
 
-#** 3) Write a Python program to extract the initials from names.
-names = ['Michael Crack', 'The Bug', 'Little Messi', 'Paul Nuts', 'Elvis Joel']
-initials = [name[0] for name in names]
-print(initials)
+#** 3) Extract the initials from names.
+# names = ['Michael', 'Bug', 'Messi', 'Paul', 'Elvis']
+# initials = [name[0] for name in names]
+# print(initials)
+
+#** 4) Filter strings by length
+# words = ['money', 'so', 'they', 'say', 'is', 'the', 'root', 'of', 'all', 'evil', 'today']
+# small_words = [word for word in words if len(word) < 4]
+# print(small_words)
+
+#** 5) Create a list of tuples
+# numbers = [1, 2, 3, 4, 5]
+# list_of_tuples = [(n, n+1, n+2) for n in numbers]
+# print(list_of_tuples)
