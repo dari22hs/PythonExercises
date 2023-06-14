a26d5513665f1524530ed68e57db85438960fb3d
Dari Huallpa <dariojoel152@gmail.com>
2023-06-14 10:29:33 -0600

    - Added exercises

--- error_handling.py
+++ error_handling.py
@@ -14,14 +14,44 @@ Error handling in Python involves handling and managing exceptions that occur du
 #     print("Error. Invalid input.")
 
 #** 2) Handling multiple exceptions
+# try:
+#     file = open("data.txt", "r")
+#     contents = file.read()
+#     file.close()
+#     print("File contents:", contents)
+# except FileNotFoundError:
+#     print("Error: File not found.")
+# except PermissionError:
+#     print("Error: Permission denied to access the file.")
+# except Exception as e:
+#     print("Error:", str(e))
+
+#** 3) Using else and finally clauses
+# try:
+#     num1 = int(input("Enter a number: "))
+#     num2 = int(input("Enter another number: "))
+#     result = num1 / num2
+# except ValueError:
+#     print("Error. Invalid input.")
+# except ZeroDivisionError:
+#      print("Error. Can't divide by zero (0).")
+# else:
+#     print(f"Result:  {result}")          
+# finally:
+#     print("End of program.")
+    
+#** 4) Raising a custom exception
+def calculate_factorial(n):
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    return factorial
+
 try:
-    file = open("data.txt", "r")
-    contents = file.read()
-    file.close()
-    print("File contents:", contents)
-except FileNotFoundError:
-    print("Error: File not found.")
-except PermissionError:
-    print("Error: Permission denied to access the file.")
-except Exception as e:
-    print("Error:", str(e))
+    num = int(input("Enter a number: "))
+    result = calculate_factorial(num)
+    print(f"Factorial: {result}")
+except ValueError as e:
+    print(f"Error: {str(e)}")
